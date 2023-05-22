21f5885cc82e7b528fe6e66fb16cb11c6a6335b2
Ali Siahi <51232067+alisiahi@users.noreply.github.com>
2023-05-22 14:46:51 +0330

    Update settings.py
    Debug False

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^l#!lz9a7)l1p9_%uqsx(cxd$uhrpnp8_h*wc9euxuy$w$g+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
