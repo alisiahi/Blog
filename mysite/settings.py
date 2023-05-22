6601a25395dbbd851f22496b0997642ed09792bd
alisiahi <alisiahi.1991@gmail.com>
2023-05-22 11:53:30 +0330

    commit before push

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,6 +148,6 @@ STATICFILES_DIRS = (
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alisiahi.1991@gmail.com'
-EMAIL_HOST_PASSWORD = 'akirzenqhhymfdmp'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
